8769fa82e7fb90425c39d91c9d62451674594606
Igor Junio <igorjunio.inf@gmail.com>
2024-09-17 19:17:54 -0300

    Site atualizado

---
+++ assets/js/darkmode.js
@@ -0,0 +1,6 @@
+const chk = document.getElementById('chk')
+    const html = document.querySelector('html')
+
+    chk.addEventListener('change', () => {
+      html.classList.toggle('dark-mode')
+    })
---
+++ assets/js/menu.js
@@ -0,0 +1,4 @@
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    document.querySelector(".mobile-menu").classList.toggle("show");
+  }
